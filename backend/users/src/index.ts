d49a5bb3a4b3e9a92d16d2f6b3933a72d707f8e1
kmoragap <38703421+kmoragap@users.noreply.github.com>
2025-07-13 18:54:13 +0200

    added post and get request in the index.ts and password is hashed

--- backend/users/src/index.ts
+++ backend/users/src/index.ts
@@ -1,15 +1,49 @@
-import fastify from 'fastify';
+import fastify from "fastify";
+import * as bcrypt from "bcrypt"
+import { PrismaClient } from "@prisma/client";
+
+//typed client based on my schema
+const prisma = new PrismaClient();
 
 const server = fastify({ logger: true });
 
-server.get('/', async (request, reply) => {
-  return { message: 'Users service is up!' } as const;
+server.get("/", async (request, reply) => {
+  return { message: "Users service is up!" } as const;
+});
+
+
+// FOR THE USER STUFF
+server.post("/users", async (request, reply) => {
+  const { username, email, password } = request.body as {
+    username: string;
+    email: string;
+    password: string;
+  };
+  try{
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { username, email, password: hashedPassword },
+    });
+    return {id: user.id, username: user.username};
+
+  }
+  catch (error){
+    console.log("Error creating the user: ", error);
+    reply.code(500).send({error: "Failed to create user. Please try again."});
+  }
+});
+
+server.get('/users', async(request, reply) => {
+  const users = await prisma.user.findMany({
+    select: {id: true, username: true, email: true}, //not necessary to put the pass field bc is not secure
+  });
+  return users;
 });
 
 const start = async () => {
   try {
-    await server.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('Users service running on port 3000');
+    await server.listen({ port: 3000, host: "0.0.0.0" });
+    console.log("Users service running on port 3000");
   } catch (err) {
     server.log.error(err);
     process.exit(1);
@@ -17,3 +51,7 @@ const start = async () => {
 };
 
 start();
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});

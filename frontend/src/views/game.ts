5efd104b8a1e95e92b61679eb26876dfb45bbb5f
grewal <der.grex@eclipso.at>
2025-08-14 11:14:07 +0200

    alpha version of game

--- frontend/src/views/game.ts
+++ frontend/src/views/game.ts
@@ -3,7 +3,10 @@ export function renderGame(): HTMLElement {
   section.className = 'flex flex-col w-full h-full absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 items-center justify-center text-center z-[3] font-[mclaren]';
 
   section.innerHTML = `
-    <img src="/assets/img/pong.png" alt="Game Image" class="w-[500px] h-[300px] mb-[20px]">
+    <!--img src="/assets/img/pong.png" alt="Game Image" class="w-[500px] h-[300px] mb-[20px]"-->
+	<canvas id = "board" width = "400" height = "300"></canvas>
+		<br>
+		<textarea id = "p1score" name = "p1score" rows = "1" cols = "2">0</textarea>:<textarea id = "p2score" name = "p2score" rows = "1" cols = "2">0</textarea>
   `;
   return section;
 }

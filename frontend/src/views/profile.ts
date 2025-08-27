baa06cb5fc063f12d5fb094af38c0b8a6f1db1bb
dvaisman <vaisman.david91@gmail.com>
2025-08-27 11:50:59 +0200

    myprofile page is not accessable if person is not logged in

---
+++ frontend/src/views/profile.ts
@@ -0,0 +1,16 @@
+export function renderHome(): HTMLElement {
+  const section = document.createElement('section');
+  section.className = 'flex flex-col w-full h-full absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 items-center justify-center text-center z-[3] font-[pressstart2p]';
+
+  section.innerHTML = `
+    <div class="title">
+      <span class="first_line"></span>
+      <span class="mid_line">
+        <span class="fast-flicker">PONG</span>
+        GA<span class="flicker">M</span>E
+      </span>
+      <span class="last_line"></span>
+    </div>
+  `;
+  return section;
+}
